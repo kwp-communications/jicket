43f331f6a7809d0e856dc29fd9d0028ba24a8746
DrLuke <Lukas@Jackowski.de>
2018-09-26 18:19:34 +0200

    Add success to log module

--- jicket/jicket/log.py
+++ jicket/jicket/log.py
@@ -3,6 +3,9 @@
 def info(msg: str) -> None:
     print("\33[1m\33[34mInfo:\33[0m %s" % msg)
 
+def success(msg: str) -> None:
+    print("\33[1m\33[32mSuccess:\33[0m %s" % msg)
+
 def warning(msg: str) -> None:
     print("\33[1m\33[33mWarning:\33[0m %s" % msg)
 
