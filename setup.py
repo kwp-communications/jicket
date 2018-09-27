7bc09fdc6c3c5691dd31b8228f2df00e6d304300
DrLuke <Lukas@Jackowski.de>
2018-09-27 11:39:45 +0200

    Update setup.py for release

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from distutils.core import setup
 
 setup(
     name='Jicket',
-    version='0.1dev',
+    version='0.1.0',
     package_dir={'': 'jicket'},
     packages=['jicket'],
     license='MIT',
     long_description=open('README.md').read(),
+    install_requires=[
+        "hashids>=1,<2",
+        "jira>=2",
+        "html2text"
+    ]
 )
